f2dccc23de958f536469acc2b659342a1c20981d
RedMadda <maddalenarossaro91@gmail.com>
2021-05-27 16:56:40 +0200

    con sconto

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
 // Il programma dovrà chiedere all'utente il 
 // numero di chilometri che vuole percorrere
 
-var km = Math.round(prompt("Inserisci quanti chilometri vuoi percorrere, per favore."));
+var km = Math.floor(prompt("Inserisci quanti chilometri vuoi percorrere, per favore."));
 
 // e l'età del passeggero.
 
-var dataNascita = Math.floor(prompt("Bene! Ora inserisci la tua data di nascita, per favore!"));
-var d = new Date();
-var giorno = d.getDate();
+var eta = Math.floor(prompt("Bene! Ora inserisci quanti anni hai, per favore!"));
+
+// Sulla base di queste informazioni dovrà 
+// calcolare il prezzo totale del viaggio, 
+// secondo queste regole: il prezzo del biglietto
+// è definito in base ai km (0.21 € al km) va 
+// applicato uno sconto del 20% per i minorenni 
+// va applicato uno sconto del 40% per gli over
+//  65.
+
+var prezzo = km * 0.21;
+if(eta >= 65){
+    var prezzo = Math.round(prezzo - prezzo * 0.4);
+} else if(eta < 18) {
+    var prezzo = Math.round(prezzo - prezzo * 0.2);
+
+}
+console.log(prezzo);
