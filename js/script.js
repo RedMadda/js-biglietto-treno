934786f654f8e5389bf444708a6e132cfccdd483
RedMadda <maddalenarossaro91@gmail.com>
2021-05-27 16:33:18 +0200

    var km

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Il programma dovrà chiedere all'utente il 
+// numero di chilometri che vuole percorrere
+
+var km = Math.round(prompt("Inserisci quanti chilometri vuoi percorrere, per favore."));
+
+// e l'età del passeggero.
+
+var dataNascita = Math.floor(prompt("Bene! Ora inserisci la tua data di nascita, per favore!"));
+var d = new Date();
+var giorno = d.getDate();
